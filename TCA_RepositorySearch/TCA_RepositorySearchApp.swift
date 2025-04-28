0c23e06576b9da15f81a5afd0c596e58b3e0fe71
sora5042 <esuora5042@gmail.com>
2025-04-28 18:31:44 +0900

    Initial Commit

---
+++ TCA_RepositorySearch/TCA_RepositorySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_RepositorySearchApp.swift
+//  TCA_RepositorySearch
+//
+//  Created by Sora Oya on 2025/04/28.
+//
+
+import SwiftUI
+
+@main
+struct TCA_RepositorySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA_RepositorySearchTests/TCA_RepositorySearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_RepositorySearchTests.swift
+//  TCA_RepositorySearchTests
+//
+//  Created by Sora Oya on 2025/04/28.
+//
+
+import Testing
+@testable import TCA_RepositorySearch
+
+struct TCA_RepositorySearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
